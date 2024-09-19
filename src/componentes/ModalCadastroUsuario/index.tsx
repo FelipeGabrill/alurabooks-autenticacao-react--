f0cba7c45f4f6458a8f7f9ede0c5f84289b0150e
FelipeGabrill <felipegabrielodss@gmail.com>
2024-09-19 18:34:46 -0300

    Finalizando a modal

--- src/componentes/ModalCadastroUsuario/index.tsx
+++ src/componentes/ModalCadastroUsuario/index.tsx
@@ -2,6 +2,8 @@ import { AbBotao, AbCampoTexto, AbModal } from "ds-alurabooks";
 import imagemPrincipal from './assets/login.png';
 import { useState } from "react";
 
+import './ModalCadastroUsuario.css';
+
 const ModalCadastroUsuario = () => {
 
     const [nome, setNome] = useState('');
@@ -12,55 +14,56 @@ const ModalCadastroUsuario = () => {
     const [senha, setSenha] = useState('');
     const [confirmarSenha, setConfirmarSenha] = useState('');
 
-
-    return (<AbModal 
-    titulo="Cadastrar" 
-    aberta={true}
-    aoFechar={() => console.log('Fechar')}
+    return (<AbModal
+        titulo="Cadastrar"
+        aberta={true}
+        aoFechar={() => console.log('Fechar')}
     >
-        <figure>
-            <img src={imagemPrincipal} alt="Monitor com fechadura e uma pessoa com uma chave"/> 
-        </figure>
-        <form>
-            <AbCampoTexto 
-                value={nome} 
-                label='Nome'
-                onChange={setNome} 
-            />
-            <AbCampoTexto 
-                value={email} 
-                label='Email'
-                onChange={setEmail} 
-            />
-            <AbCampoTexto 
-                value={endereco} 
-                label='Endereco'
-                onChange={setEndereco} 
-            />
-            <AbCampoTexto 
-                value={complemento} 
-                label='Complemento'
-                onChange={setComplemento} 
-            />
-            <AbCampoTexto 
-                value={cep} 
-                label='CEP'
-                onChange={setCep} 
-            />
-            <AbCampoTexto 
-                value={senha} 
-                label='Senha'
-                onChange={setSenha} 
-            />
-            <AbCampoTexto 
-                value={confirmarSenha} 
-                label='Confirmar Senha'
-                onChange={setConfirmarSenha} 
-            />
-        </form>
-        <footer>
-            <AbBotao texto='Cadastrar' />
-        </footer>
+        <div className='corpoModalCadastro'>
+            <figure>
+                <img src={imagemPrincipal} alt="Monitor com fechadura e uma pessoa com uma chave" />
+            </figure>
+            <form>
+                <AbCampoTexto
+                    value={nome}
+                    label='Nome'
+                    onChange={setNome}
+                />
+                <AbCampoTexto
+                    value={email}
+                    label='Email'
+                    onChange={setEmail}
+                />
+                <AbCampoTexto
+                    value={endereco}
+                    label='Endereco'
+                    onChange={setEndereco}
+                />
+                <AbCampoTexto
+                    value={complemento}
+                    label='Complemento'
+                    onChange={setComplemento}
+                />
+                <AbCampoTexto
+                    value={cep}
+                    label='CEP'
+                    onChange={setCep}
+                />
+                <AbCampoTexto
+                    value={senha}
+                    label='Senha'
+                    onChange={setSenha}
+                />
+                <AbCampoTexto
+                    value={confirmarSenha}
+                    label='Confirmar Senha'
+                    onChange={setConfirmarSenha}
+                />
+                <footer>
+                    <AbBotao texto='Cadastrar' />
+                </footer>
+            </form>
+        </div>
     </AbModal>)
 }
 
