2be42d7a6a63ae16cc61e7e637641925da389fa5
FelipeGabrill <felipegabrielodss@gmail.com>
2024-09-19 21:56:08 -0300

    Página de pedidos

---
+++ src/paginas/Pedidos/index.tsx
@@ -0,0 +1,20 @@
+import { AbBotao } from "ds-alurabooks"
+
+const Pedidos = () => {
+    return (
+        <section className="pedidos">
+            <h1>Meus Pedidos</h1>
+            <div>
+                <ul>
+                    <li>Pedido: <strong>12345</strong></li>
+                    <li>Data do pedido: <strong>22/11/2024</strong></li>
+                    <li>Valor total: <strong>R$ 42.00</strong></li>
+                    <li>Entrega realizada em: <strong>25/11/2024</strong></li>
+                </ul>
+                <AbBotao texto="Detalhes"/>
+            </div>
+        </section>
+    )
+}
+
+export default Pedidos
