53054180f806e4a321d9e5f2ce701415f5387126
FelipeGabrill <felipegabrielodss@gmail.com>
2024-09-19 23:06:56 -0300

    Exibindo pedidos

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -1,62 +1,39 @@
 import { AbBotao } from "ds-alurabooks"
 import './Pedidos.css'
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { IPedido } from "../../interfaces/IPedido"
 
 const Pedidos = () => {
 
+    const formatador = Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' })
+
+    const [pedidos,setPedidos] = useState<IPedido[]>([])
+
     useEffect(() => {
-
         const token = sessionStorage.getItem('token')
-
         axios.get<IPedido[]>('http://localhost:8000/pedidos', {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
-        }).then(resposta => console.log(resposta.data))
+        }).then(resposta => setPedidos(resposta.data))
         .catch(erro => console.log(erro))
     }, [])
 
     return (
         <section className="pedidos">
             <h1>Meus Pedidos</h1>
-            <div className="pedido">
+            {pedidos.map(pedido => (
+            <div className="pedido" key={pedido.id}>
                 <ul>
-                    <li>Pedido: <strong>12345</strong></li>
-                    <li>Data do pedido: <strong>22/11/2024</strong></li>
-                    <li>Valor total: <strong>R$ 42.00</strong></li>
-                    <li>Entrega realizada em: <strong>25/11/2024</strong></li>
+                    <li>Pedido: <strong>{pedido.id}</strong></li>
+                    <li>Data do pedido: <strong>{new Date(pedido.data).toLocaleDateString()}</strong></li>
+                    <li>Valor total: <strong>{formatador.format(pedido.total)}</strong></li>
+                    <li>Entrega realizada em: <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong></li>
                 </ul>
                 <AbBotao texto="Detalhes"/>
             </div>
-            <div className="pedido">
-                <ul>
-                    <li>Pedido: <strong>12345</strong></li>
-                    <li>Data do pedido: <strong>22/11/2024</strong></li>
-                    <li>Valor total: <strong>R$ 42.00</strong></li>
-                    <li>Entrega realizada em: <strong>25/11/2024</strong></li>
-                </ul>
-                <AbBotao texto="Detalhes"/>
-            </div>
-            <div className="pedido">
-                <ul>
-                    <li>Pedido: <strong>12345</strong></li>
-                    <li>Data do pedido: <strong>22/11/2024</strong></li>
-                    <li>Valor total: <strong>R$ 42.00</strong></li>
-                    <li>Entrega realizada em: <strong>25/11/2024</strong></li>
-                </ul>
-                <AbBotao texto="Detalhes"/>
-            </div>
-            <div className="pedido">
-                <ul>
-                    <li>Pedido: <strong>12345</strong></li>
-                    <li>Data do pedido: <strong>22/11/2024</strong></li>
-                    <li>Valor total: <strong>R$ 42.00</strong></li>
-                    <li>Entrega realizada em: <strong>25/11/2024</strong></li>
-                </ul>
-                <AbBotao texto="Detalhes"/>
-            </div>
+            ))}
         </section>
     )
 }
