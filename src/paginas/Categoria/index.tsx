53bbb414f2cda4a6dc1712137841975a0f6253c0
FelipeGabrill <felipegabrielodss@gmail.com>
2024-09-22 20:18:31 -0300

    Página base

---
+++ src/paginas/Categoria/index.tsx
@@ -0,0 +1,12 @@
+import TituloPrincipal from "../../componentes/TituloPrincipal"
+
+const Categoria = () => {
+    return (
+        <section>
+            <TituloPrincipal texto="Categoria" />
+        </section>
+    )
+
+}
+
+export default Categoria
