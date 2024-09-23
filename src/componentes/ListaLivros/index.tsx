f37f13d35c24128e8b52988ec2ae23880325bd68
FelipeGabrill <felipegabrielodss@gmail.com>
2024-09-23 20:20:14 -0300

    Passando parâmetros

--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -9,8 +9,8 @@ interface ListaLivroProps {
 }
 
 const OBTER_LIVROS = gql`
-    query ObterLivros {
-        livros {
+    query ObterLivros($categoriaId: Int) {
+        livros(categoriaId: $categoriaId) {
             id,
             slug,
             titulo,
@@ -27,7 +27,11 @@ const ListaLivros = ({ categoria } : ListaLivroProps) => {
 
     // const { data: produtos } = useQuery({queryKey: ['buscaLivrosPorCategoria', categoria], queryFn: () => obterProdutosDaCategoria(categoria)})
 
-    const { data } = useQuery<{livros : ILivro[]}>(OBTER_LIVROS)
+    const { data } = useQuery<{livros : ILivro[]}>(OBTER_LIVROS, {
+        variables: {
+            categoriaId: categoria.id
+        }
+    })
 
     return <section className="livros">
         {data?.livros.map((livro: ILivro) => <CardLivro livro={livro} key={livro.id}/>)}
