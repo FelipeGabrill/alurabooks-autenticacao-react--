b05875aa6fda5da390aad8629eeec214b89cdf98
FelipeGabrill <felipegabrielodss@gmail.com>
2024-09-22 22:29:58 -0300

    Extraindo a chamada HTTP

--- src/paginas/Categoria/index.tsx
+++ src/paginas/Categoria/index.tsx
@@ -1,33 +1,15 @@
-import { useEffect, useState } from "react"
 import TituloPrincipal from "../../componentes/TituloPrincipal"
-import { ICategoria } from "../../interfaces/ICategoria"
-import http from "../../http"
+import { obterCategoriaPorSlug } from "../../http"
 import { useParams } from "react-router-dom"
 import Loader from "../../componentes/Loader"
+import { useQuery } from "@tanstack/react-query"
 
 const Categoria = () => {
 
-    const [categoria, setCategoria] = useState<ICategoria>()
-    const [estaCarregando, setEstaCarregando] = useState(true)
-
     const params = useParams()
+    const { data: categoria, isLoading } = useQuery({queryKey: ['categoriaPorSlug'], queryFn: () => obterCategoriaPorSlug(params.slug || '')})
 
-    useEffect(() => {
-        setEstaCarregando(true)
-        http.get<ICategoria[]>('categorias', {
-            params: {
-                slug: params.slug
-            }    
-        }).then(resposta => {
-            setCategoria(resposta.data[0])
-            setEstaCarregando(false)
-        }).catch(erro => {
-            console.log(erro)
-            setEstaCarregando(false)
-        })
-    }, [params.slug])
-
-    if (estaCarregando) {
+    if (isLoading) {
         return <Loader />
     }
 
