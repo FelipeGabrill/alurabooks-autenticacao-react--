7f4d19c06ada4d4fdb79c6b7118e00601cfe2be3
FelipeGabrill <felipegabrielodss@gmail.com>
2024-09-27 19:28:03 -0300

    Adicionando itens

--- src/contextApi/carrinho/index.tsx
+++ src/contextApi/carrinho/index.tsx
@@ -1,11 +1,16 @@
+import { useAdicionarItem, useCarrinho } from "../../graphql/carrinho/hooks";
 import { ICarrinho } from "../../interfaces/ICarrinho";
 import { createContext, ReactElement, useContext } from "react";
+import { IItemCarrinho } from "../../interfaces/IItemCarrinho";
 
 export interface ICarrinhoContext {
     carrinho?: ICarrinho
+    adicionarItemCarrinho: (item: IItemCarrinho) => void
 }
 
-export const CarrinhoContext = createContext<ICarrinhoContext>({})
+export const CarrinhoContext = createContext<ICarrinhoContext>({
+    adicionarItemCarrinho: () => null
+})
 
 interface CarrinhoProviderProps {
     children: ReactElement
@@ -13,14 +18,26 @@ interface CarrinhoProviderProps {
 
 const CarrinhoProvider = ({ children }: CarrinhoProviderProps) => {
 
-    const carrinho: ICarrinho = {
-        itens: [],
-        total: 0
+    const { data } = useCarrinho()
+
+    const [adicionaItem] = useAdicionarItem()
+
+    const adicionarItemCarrinho = (item:IItemCarrinho) => {
+        //console.log('[CarrinhoProvider] - adicionarItemCarrinho ', item)
+        adicionaItem({
+            variables: {
+                item: {
+                    livroId: item.livro.id,
+                    opcaoCompraId: item.opcaoCompra.id,
+                    quantidade: item.quantidade
+                }
+            }
+        })
     }
 
     return (
 
-        <CarrinhoContext.Provider value={{ carrinho }}>
+        <CarrinhoContext.Provider value={{ carrinho: data?.carrinho, adicionarItemCarrinho }}>
             {children}
         </CarrinhoContext.Provider>
     )
